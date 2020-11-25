193ad5ca2939457bcbec296f6b0e4dca3446be96
socratone <gim2origin@gmail.com>
2020-11-25 17:06:12 +0900

    비어 있는 재생리스트도 불러올 수 있게 수정

--- src/getPlaylist.js
+++ src/getPlaylist.js
@@ -8,37 +8,61 @@ router.get('/', (req, res) => {
   let sql = `
     SELECT playlist.id, playlist.title, video.videoId, video.title videoTitle FROM playlist_video
     INNER JOIN playlist ON playlist.id = playlist_video.pId
-    INNER JOIN video ON video.id = playlist_video.vId`;
+    INNER JOIN video ON video.id = playlist_video.vId
+    ORDER BY playlist.id`;
+    // 재생리스트와 재생리스트에 들어 있는 곡 전부를 rows로 가져온다.
 
   new Promise((resolve, reject) => {
-    db.all(sql, [], (error, datas) => {
+    db.all(sql, [], (error, rows) => {
       if (error) reject(error);
-      resolve(datas);
+      resolve(rows);
     });
-  }).then(datas => {
-    // console.log('datas:', datas);
-    db.close();
-
-    let listIdIndex = 0;
+  }).then(rows => {
+    console.log('rows1:', rows);
     let results = [];
-
-    for (let i = 0; i < datas.length; i++) {
-      if (datas[i].id > listIdIndex) { // 새로운 id가 나왔을 때 객체를 새로 생성
-        listIdIndex++;
+    let lastId = 0;
+    for (let i = 0; i < rows.length; i++) {
+      if (rows[i].id !== lastId) { // 새로운 id가 나왔을 때 객체를 새로 생성
+        lastId = rows[i].id;
         let playlist = {
-          listId: listIdIndex,
-          title: datas[i].title,
+          listId: lastId,
+          title: rows[i].title,
           videos: []
         };
         results.push(playlist);
       }
       const result = results[results.length - 1];
       result.videos.push({ 
-        videoId: datas[i].videoId, 
-        title: datas[i].videoTitle 
+        videoId: rows[i].videoId, 
+        title: rows[i].videoTitle 
       });
     }
-    
+    return results;
+  }).then(results => {
+    let sql = `
+      SELECT playlist.id, playlist.title FROM playlist
+      LEFT OUTER JOIN playlist_video ON playlist.id = playlist_video.pId
+      WHERE playlist_video.pId IS NULL`;
+      // playlist_video.pId에 없는 playlist.id rows를 찾는다. (= 비어 있는 재생리스트)
+
+    return new Promise((resolve, reject) => {
+      db.all(sql, [], (error, rows) => {
+        if (error) reject(error);
+        resolve(rows);
+      });
+    }).then(rows => {
+      console.log('rows2:', rows)
+      db.close();
+      rows.forEach(row => {
+        results.push({
+          listId: row.id,
+          title: row.title,
+          videos: [],
+        });
+      });
+      return results;
+    });
+  }).then(results => {
     res.status(200).send(results);
   }).catch(error => {
     res.status(500).send(error);
