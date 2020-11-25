748a65994b02be67606e8d88a0d3993d20cda83e
socratone <gim2origin@gmail.com>
2020-11-26 03:08:42 +0900

    routes 설정

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const getPlaylist = require('./getPlaylist');
-const postNewPlaylist = require('./postNewPlaylist');
+const routes = require('./routes');
 
 app.use(cors());
 app.use(express.json()); 
  
-app.use('/api/playlist', getPlaylist);
-app.use('/api/new-playlist', postNewPlaylist);
+app.use('/api/playlist', routes.getPlaylist);
+app.use('/api/new-playlist', routes.postNewPlaylist);
 
 app.listen(4000, () => console.log('서버가 4000 포트에서 가동 중입니다.'));
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+const getPlaylist = require('./getPlaylist');
+const postNewPlaylist = require('./postNewPlaylist');
+
+module.exports = {
+  getPlaylist,
+  postNewPlaylist
+}
