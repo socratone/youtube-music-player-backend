99c86a4fe41bcca90a8e3e407a74a27126b0ff1e
socratone <gim2origin@gmail.com>
2020-11-25 18:47:23 +0900

    api url 수정

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const postNewPlaylist = require('./postNewPlaylist');
 app.use(cors());
 app.use(express.json()); 
  
-app.use('/api/playlist_video', getPlaylist);
-app.use('/api/playlist_video', postNewPlaylist);
+app.use('/api/playlist', getPlaylist);
+app.use('/api/new-playlist', postNewPlaylist);
 
 app.listen(4000, () => console.log('서버가 4000 포트에서 가동 중입니다.'));
