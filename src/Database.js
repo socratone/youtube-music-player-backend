74f3b4fb96bdacad97cf5df3159ac99954b406ca
socratone <gim2origin@gmail.com>
2020-11-26 12:57:43 +0900

    database oop 구현

---
+++ src/Database.js
@@ -0,0 +1,31 @@
+const sqlite3 = require('sqlite3').verbose();
+
+class Database {
+  load(path) {
+    return new Promise((resolve, reject) => { 
+      this.db = new sqlite3.Database(path, error => {
+        if (error) reject(error);
+        resolve();
+      });
+    }).catch(error => {
+      throw error;
+    });
+  }
+
+  run(sql, params) { // sql: string, params: [string]
+    return new Promise((resolve, reject) => {
+      this.db.run(sql, params, function (error) {
+        if (error) reject(error);
+        resolve(this.lastID.toString());
+      });
+    }).catch(error => {
+      throw error;
+    });
+  }
+
+  close() {
+    this.db.close();
+  }
+}
+
+module.exports = Database;
