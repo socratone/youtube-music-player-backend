a706c9b27660ed5357b844cfc264128af09f6126
socratone <gim2origin@gmail.com>
2020-11-27 13:35:17 +0900

    중복된 use 삭제

--- src/index.js
+++ src/index.js
@@ -8,6 +8,5 @@ app.use(cors());
 app.use(express.json()); 
  
 app.use('/playlist', routes.playlist);
-app.use('/playlist', routes.playlist);
 
 app.listen(4000, () => console.log('서버가 4000 포트에서 가동 중입니다.'));
